7df7271a97b3dd6d935040fee4bfc5c9eb6f2a8b
Susan Mwiberi <smwiberi@safaricom.co.ke>
2024-02-29 09:03:41 +0300

    day 4

--- week2/constructors.js
+++ week2/constructors.js
@@ -5,16 +5,23 @@
 // console.log(name.valueOf());
 
 // create Student constructor using  a function
-function Student(name, age, gender) {
+function Student(name, age, gender, grade) {
     this.name = name;
     this.age = age;
     this.gender = gender;
+    this.grade = grade;
 
     this.speak = () => {
         return 'my name is ' + name;
     }
     
 }
-const studenta = new Student('sue', 5,  'female');
+const studenta = new Student('sue', 5,  'female', 'A');
 console.log("studenta details", studenta);
 console.log(studenta.speak());
+
+// another way to creating a function for a Student Object
+Student.prototype.getGrader = function() {
+    return this.grade;
+}
+console.log("The student grade is " + studenta.getGrader());
