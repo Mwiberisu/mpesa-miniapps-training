f5d6e6170fdd6a5e213ff4e85f716c46e12eeebb
Susan Mwiberi <susan.mwiberi@gmail.com>
2024-04-17 09:11:45 +0300

    Enabled all tests to reuse the same Triangle object

--- week4/assignment/__tests__/triangle.test.js
+++ week4/assignment/__tests__/triangle.test.js
@@ -2,26 +2,29 @@ import Triangle from "./../src/triangle.js";
 
 describe("Triangle Tests", () => {
   //group triangle tests in a suite
+  let reusableTriangle;
+  beforeEach(() => {
+    reusableTriangle = new Triangle(5, 5, 5);
+  });
   test("should correctly create a triangle with 3 lengths", () => {
-    const triangle = new Triangle(2, 4, 6);
-    expect(triangle.side1).toEqual(2);
-    expect(triangle.side2).toEqual(4);
-    expect(triangle.side3).toEqual(6);
+    expect(reusableTriangle.side1).toEqual(5);
+    expect(reusableTriangle.side2).toEqual(5);
+    expect(reusableTriangle.side3).toEqual(5);
   });
   test("should correctly determine if 3 lengths are not a triangle", () => {
-    const notTriangle = new Triangle(2, 7, 3);
-    expect(notTriangle.checkType()).toEqual("not a triangle");
+    reusableTriangle.side1 = 20;
+    expect(reusableTriangle.checkType()).toEqual("not a triangle");
   });
   test("should correctly determine whether three lengths make a scalene triangle", () => {
-    const scalTriangle = new Triangle(4, 5, 7);
-    expect(scalTriangle.checkType()).toEqual("scalene triangle");
+    reusableTriangle.side2 = 6;
+    reusableTriangle.side3 = 7;
+    expect(reusableTriangle.checkType()).toEqual("scalene triangle");
   });
   test("should correctly determine whether three lengths make an isosceles triangle", () => {
-    const isoscTriangle = new Triangle(5, 5, 7);
-    expect(isoscTriangle.checkType()).toEqual("isosceles triangle");
+    reusableTriangle.side1 = 7;
+    expect(reusableTriangle.checkType()).toEqual("isosceles triangle");
   });
   test("should correctly determine whether three lengths make an equilateral triangle", () => {
-    const isoscTriangle = new Triangle(5, 5, 5);
-    expect(isoscTriangle.checkType()).toEqual("equilateral triangle");
+    expect(reusableTriangle.checkType()).toEqual("equilateral triangle");
   });
 });
