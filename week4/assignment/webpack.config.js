1f074cbfd5a0c0a3c3aebd640e629c4bdfb7ac84
Susan Mwiberi <susan.mwiberi@gmail.com>
2024-04-17 07:55:39 +0300

    Added eslint

--- week4/assignment/webpack.config.js
+++ week4/assignment/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -15,6 +16,7 @@ module.exports = {
   }, //
   devtool: "eval-source-map",
   plugins: [
+    new ESLintPlugin(),
     new CleanWebpackPlugin({
       verbose: true,
     }),
