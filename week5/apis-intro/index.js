4a64054442a23475b8e0ec5b12a2f14cb7c94611
Susan Mwiberi <smwiberi@safaricom.co.ke>
2024-04-30 09:38:16 +0300

    Sample post request

--- week5/apis-intro/index.js
+++ week5/apis-intro/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 app.get('/', function (req, res) {
   console.log("heyy!");
   res.send('Hello World')
 })
+
+app.post('/intro', function (req, res) {
+  console.log(req.query)
+  console.log(req.body)
+  res.json({message:'Request hit the POST endpoint successfully',data:req.body});
+});
 
 app.listen(3000, function () {console.log('App listening on port 3000')});
