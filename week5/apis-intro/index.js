f4830ae3f6e427ca950ce0734d694dd864b0a76f
Susan Mwiberi <smwiberi@safaricom.co.ke>
2024-04-30 09:14:09 +0300

    Added a simple API

--- week5/apis-intro/index.js
+++ week5/apis-intro/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
+  console.log("heyy!");
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(3000, function () {console.log('App listening on port 3000')});
