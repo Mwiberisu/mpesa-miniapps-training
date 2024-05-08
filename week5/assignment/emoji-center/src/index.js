8a518fdd54334847d920ec3eb7c292883ccd05c8
Susan Mwiberi <smwiberi@safaricom.co.ke>
2024-05-08 14:19:29 +0300

    Added styling

--- week5/assignment/emoji-center/src/index.js
+++ week5/assignment/emoji-center/src/index.js
@@ -5,7 +5,6 @@ let categories = [
   "travel-and-places",
   "activities",
   "objects",
-  "people",
   "symbols",
   "flags",
 ];
@@ -35,11 +34,29 @@ async function loadPage() {
 
     if (emojiResponse.status === 200) {
       const emojis = emojiResponse.data;
-      const recipeList = document.getElementById("emojiList");
+      const emojiList = document.getElementById("emojiList");
       for (const emoji of emojis) {
-        const emj = document.createElement("p");
-        emj.innerHTML = emoji.htmlCode[0];
-        emojiList.appendChild(emj);
+        const theList = document.createElement("li");
+        const emojiDiv = document.createElement("div");
+        emojiDiv.className = "class-div-1";
+        const emjImg = document.createElement("p");
+        emjImg.innerHTML = emoji.htmlCode[0];
+        const emjName = document.createElement("p");
+        let theName = emoji.name.toLowerCase();
+        if (theName.includes("≊")) {
+          theName = theName.substring(theName.indexOf("≊") + 1, theName.length);
+        }
+        if (theName.includes("face with ")) {
+          theName = theName.substring(
+            theName.indexOf("face with ") + 10,
+            theName.length
+          );
+        }
+        emjName.innerHTML = theName;
+        emojiDiv.appendChild(emjImg);
+        emojiDiv.appendChild(emjName);
+        theList.appendChild(emojiDiv);
+        emojiList.appendChild(theList);
       }
     } else {
       document.getElementById("error").innerHTML = "Unable to fetch emojis";
