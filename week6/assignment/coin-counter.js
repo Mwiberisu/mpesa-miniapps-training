0827e6f492bd6a87910bfd12342f69bb04b65a1d
Susan Mwiberi <susan.mwiberi@gmail.com>
2024-06-10 09:25:12 +0300

    intro to miniapps

---
+++ week6/assignment/coin-counter.js
@@ -0,0 +1,15 @@
+/*
+Coin Counter
+Create a coin counter application that takes X amount of money (such as Ksh. 499) and determines the exact amount of change needed in 1's, 5's, 10's, 20's, 50's and 100's. Do not worry about adding a user interface to the application. Instead, focus on writing good tests and functional code.
+400=>4
+50 =>1
+20 =>2
+5 =>1
+1 =>4
+Part 1
+Create a coin counter function that uses recursion to solve the problem.
+*/
+
+/*Part 2
+Create a coin counter function that uses a closure that can be used with functions for each type of change (quarters, nickels, dimes and pennies). You can use recursion if you like.
+*/
